f9d0d13af56b31d666abb697bdf1ab76e3a41424
Sergiu Costache <costache.sergiu@gmail.com>
2020-09-05 11:19:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,9 @@ cp .env.example .env
 docker-compose up -d
 docker-compose ps
 
+# allow mysql to boot up
+sleep 3
+
+msg="\n============ Create database =============="
+echo -e "$msg"
 docker-compose exec mysql mysql -uroot -proot --execute="CREATE DATABASE IF NOT EXISTS mgtest;"
